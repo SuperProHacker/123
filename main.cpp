a63bad3abd4b6609e72fdbd7e351da55aed454c5
Кривов Андрей Игоревич <st141679@student.spbu.ru>
2025-10-21 12:52:41 +0300

    revrsed

--- main.cpp
+++ main.cpp
@@ -1,14 +1,40 @@
 #include <iostream>
 
-int main(){
+int main(int argc, char* argv[]) {
 	unsigned int number = 0;
 	unsigned int sum = 0;
-	unsigned int prod = 0;
-	std::cout << "Input number: ";
+	unsigned int prod = 1u;
+
+	if (argc > 1) {
+		char *number = argv[1];
+		int index = 0;
+		while (ch = number[index] != '\0' && ) {
+			if number[index] >= '0' && if number[index] <= '9' ){
+			sum += (number[index] - '0');
+			prod *= (number[index] - '0'); }
+			else {
+				std::cout << "Wrong number" << argv[1] << "'" << std::endl;
+			}
+		}
+		std::cout << sum << ", " << prod << std::endl;
+		++index;
+	}
+	else {
+		
+		std::cout << "Input number: ";
+		std::cin >> number;
+	}
 	do {
-	sum += number %10;
-	prod += number %10;
-	number /=10;
-	}while (number != 0 );
-	std::cout << sum << ", "<< prod << std::endl;}
+		sum += number % 10;
+		prod += number % 10;
+		number /= 10;
+	} while (number != '0') {
+		do {
+			sum += number % 10;
+			prod += number % 10;
+			number /= 10;
+		}
+	}
+		std::cout << sum << ", " << prod << std::endl;
+}
 
