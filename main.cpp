0d6ecae7eba2bb3244293b69750909e834482e2f
lll <st>
2025-10-21 11:50:46 +0300

    add solution

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main(){
+	unsigned int number = 0;
+	unsigned int sum = 0;
+	unsigned int prod = 0;
+	std::cout << "Input number: ";
+	do {
+	sum += number %10;
+	prod += number %10;
+	number /=10;
+	}while (number != 0 );
+	std::cout << summ << ", "<< prod << std<<endl;}
+
