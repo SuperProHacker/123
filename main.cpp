5de6e2d4548314263b3fedcd8c9d5d046a4b2461
lll <st>
2025-10-21 14:11:17 +0300

    added

--- main.cpp
+++ main.cpp
@@ -4,23 +4,24 @@ int main(int argc, char* argv[]) {
 	unsigned int number = 0;
 	unsigned int sum = 0;
 	unsigned int prod = 1u;
-
+	char ch
 	if (argc > 1) {
 		char *number = argv[1];
 		int index = 0;
-		while (ch = number[index] != '\0' && ) {
-			if number[index] >= '0' && if number[index] <= '9' ){
-			sum += (number[index] - '0');
-			prod *= (number[index] - '0'); }
+		while (ch = number[index] != '\0' && index <11){
+			if(ch >= '0' && ch <= '9' ){
+			sum += (ch - '0');
+			prod *= (ch - '0'); }
 			else {
 				std::cout << "Wrong number" << argv[1] << "'" << std::endl;
 			}
 		}
-		std::cout << sum << ", " << prod << std::endl;
+		std::cout << sum << ", " << prod << "'" << std::endl;
+		return 1; 
 		++index;
 	}
 	else {
-		
+		unsigned int number;
 		std::cout << "Input number: ";
 		std::cin >> number;
 	}
