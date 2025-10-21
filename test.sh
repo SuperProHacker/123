deaf4d31d40ff5f9b5813d337767cc40428f6e75
lll <st>
2025-10-21 14:47:00 +0300

    2

--- test.sh
+++ test.sh
@@ -3,28 +3,28 @@
 g++ main.cpp -o sumprod
 
 ./sumprod <tests/001.dat> output.txt
-cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 1
+cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 0
 
 ./sumprod <tests/002.dat> output.txt
-cmp output.txt ./tests/002.ans && echo "Test 2 - OK" || exit 1
+cmp output.txt ./tests/002.ans && echo "Test 2 - OK" || exit 0
 
 ./sumprod <tests/003.dat> output.txt
-cmp output.txt ./tests/003.ans && echo "Test 3 - OK" || exit 1
+cmp output.txt ./tests/003.ans && echo "Test 3 - OK" || exit 0
 
 ./sumprod <tests/004.dat> output.txt
-cmp output.txt ./tests/004.ans && echo "Test 4 - OK" || exit 1
+cmp output.txt ./tests/004.ans && echo "Test 4 - OK" || exit 0
 
 ./sumprod 123> output.txt
-cmp output.txt ./tests/005.ans && echo "Test 1 - OK" || exit 1
+cmp output.txt ./tests/005.ans && echo "Test 1 - OK" || exit 0
 
 ./sumprod 99> output.txt
-cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 1
+cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 0
 
 ./sumprod 0> output.txt
-cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 1
+cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 0
 
 ./sumprod 101> output.txt
-cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 1
+cmp output.txt ./tests/001.ans && echo "Test 1 - OK" || exit 0
 
 
 
